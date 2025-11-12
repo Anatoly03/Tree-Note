9e4196b3e46543e1a524cccaf5c66db3901c2c7a
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-12 21:31:13 +0100

    add font awesome icons to folder content display

--- app-client/src/components/views/ViewDirectoryContent.vue
+++ app-client/src/components/views/ViewDirectoryContent.vue
@@ -1,31 +1,44 @@
 <template>
     <div class="view-directory-content">
-        <v-for :key="file.id" v-for="file in fileTree">
-            <div class="content">
-                {{ file.name }}
-            </div>
-        </v-for>
+        <div v-for="file in fileTree" :key="file.id" class="content">
+            <font-awesome-icon icon="fa-regular fa-folder" v-if="file.isDir" />
+            <font-awesome-icon icon="fa-regular fa-file" v-if="file.isFile" />
+            {{ file.name }}
+        </div>
 
         <!-- TODO remove -->
-        <router-link to="/">Back to Home</router-link>
+        <div class="footer">
+            <router-link to="/">Back to Home</router-link>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { readDir } from '@tauri-apps/plugin-fs';
+import { onMounted, ref } from "vue"
+import { useRouter } from "vue-router"
+import { readDir } from "@tauri-apps/plugin-fs"
 
-const router = useRouter();
-const fileTree = ref<any[]>([]);
+const router = useRouter()
+const fileTree = ref<any[]>([])
 
 onMounted(async () => {
-    const directory = router.currentRoute.value.params.directory as string;
-    const contents = await readDir('/' + directory);
-    console.log(contents);
+    const directory = router.currentRoute.value.params.directory as string
+    const contents = await readDir("/" + directory)
 
-    fileTree.value = contents;
-});
+    fileTree.value = []
+    for (const item of contents) {
+        if (item.name.startsWith(".")) continue
+
+        const fullPath = directory + "/" + item.name
+
+        fileTree.value.push({
+            id: fullPath,
+            name: item.name,
+            isDir: item.isDirectory,
+            isFile: item.isFile,
+        })
+    }
+})
 </script>
 
 <style lang="scss" scoped>
@@ -43,14 +56,32 @@ onMounted(async () => {
     .content {
         padding: 2px;
         border-radius: 4px;
-        
-        cursor: pointer;
+
+        cursor: not-allowed; // TODO when clickable: pointer;
         transition: background-color 0.2s ease;
 
         &:hover {
             background-color: $bg-accent-light;
         }
+
+        :deep(svg) {
+            color: $fg-accent;
+        }
+    }
+
+    .footer {
+        margin-top: auto;
+        padding-top: 10px;
+        border-top: 1px solid $border-color;
+
+        :deep(a) {
+            color: $fg-accent;
+            text-decoration: none;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
     }
 }
 </style>
-
--- app-client/src/main.ts
+++ app-client/src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
 import { createWebHistory, createRouter } from "vue-router";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 import App from "./components/App.vue";
+
+library.add(fas, far);
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,5 +16,6 @@ const router = createRouter({
 });
 
 createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
     .mount("#app");
