439444e0f0c1ec7200bf5e945c6d54662ceb8acb
Anatoly Weinstein <anatoly.wein@gmail.com>
2025-11-16 21:02:12 +0100

    add markdown wysiwig (implements full markdown reading and writing)

--- app-client/src/components/views/ViewFileEditor.vue
+++ app-client/src/components/views/ViewFileEditor.vue
@@ -6,15 +6,18 @@
 
 <script lang="ts" setup>
 import { onMounted, onUpdated, ref } from "vue";
-import { core } from "@tauri-apps/api";
-import { readFile } from "@tauri-apps/plugin-fs";
+import { readFile, writeFile } from "@tauri-apps/plugin-fs";
 import { useEditor, EditorContent } from "@tiptap/vue-3"
 import StarterKit from "@tiptap/starter-kit"
+import showdown from 'showdown';
 
 // The filepath property.
 const props = defineProps<{
     path: string | null;
 }>();
+
+// The markdown to HTML converter.
+const converter = new showdown.Converter();
 
 // The prose mirror editor instance.
 const editor = useEditor({
@@ -25,21 +28,36 @@ const editor = useEditor({
 // The currently opened file path.
 const currentFile = ref<string | null>(props.path);
 
+// Save the current file content.
+async function saveFile() {
+    const html = editor.value?.getHTML() ?? '';
+    const md = converter.makeMd(html);
+
+    // TODO if currentFile is not set, save as untitled file
+    if (!currentFile.value) return;
+
+    try {
+        const stream = new TextEncoder().encode(md);
+
+        await writeFile(currentFile.value, stream);
+    } catch (e) {
+        console.error('could not save file:', e);
+        // TODO show error to user
+    }
+}
+
 // Load note content when component is mounted. Currently, there is
 // no error handling and only one file saved.
 onMounted(async () => {
     // Save note content after every update. Currently, there is
     // no error handling and only one file saved.
-    editor.value!.on("update", async () => {
-        const content = editor.value?.getHTML() || "";
-        await core.invoke("save_note", { text: content });
-    });
+    editor.value!.on("update", () => saveFile());
 });
 
 // Update the editor content when the file path changes. This save-closes
 // the current file, updates path and reads new file.
 onUpdated(async () => {
-    // TODO save file
+    // saveFile();
     
     if (props.path === currentFile.value) return;
     // TODO close file
@@ -54,8 +72,9 @@ onUpdated(async () => {
 
     try {
         const file = await readFile(currentFile.value);
-        console.debug(file);
-        content = typeof file === "string" ? file : new TextDecoder().decode(file);
+        const text = typeof file === "string" ? file : new TextDecoder().decode(file);
+
+        content = converter.makeHtml(text);
     } catch (e) {
         content = `<p><b>Error</b>: Could not read file at path ${currentFile.value}</p>`;
         // TODO show error to user
